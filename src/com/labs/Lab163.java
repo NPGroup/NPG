70b4bf67af23f427b1ef990b8f729d6b81b8df52
NPGroup <noodpgroup@gmail.com>
2017-10-14 19:12:16 +0300

    Lab163

--- src/com/labs/Lab163.java
+++ src/com/labs/Lab163.java
@@ -1,4 +1,21 @@
 package com.labs;
 
 public class Lab163 {
+    public static void main(String[] args) {
+        int[][] array = new int[4][4];
+        int k = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[j][i] = k;
+                k++;
+            }
+        }
+
+        for (int[] w : array) {
+            for (int t : w) {
+                System.out.print(" | " + t + " | ");
+            }
+            System.out.println();
+        }
+    }
 }
