352abe68e7924fc71a2a4895fddf5d24e8912fcb
NPGroup <noodpgroup@gmail.com>
2017-10-14 22:37:15 +0300

    Lab166

--- src/com/labs/Lab166.java
+++ src/com/labs/Lab166.java
@@ -1,4 +1,13 @@
 package com.labs;
 
+import java.util.Arrays;
+import java.lang.String;
+
 public class Lab166 {
+    public static void main(String[] args) {
+        double[] array = {-25, -15, -5, 5, 17, 20, 30, 28, 18, 10, -1, -16};
+            Arrays.sort(array);
+            System.out.println(Arrays.toString(array));
+
+    }
 }
