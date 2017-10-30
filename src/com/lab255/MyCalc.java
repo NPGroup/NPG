61c8389039171823380a2c6f325b4560233b593d
NPGroup <noodpgroup@gmail.com>
2017-10-30 07:53:41 +0200

    Lab 2.4.5

--- src/com/lab255/MyCalc.java
+++ src/com/lab255/MyCalc.java
@@ -1,4 +1,33 @@
 package com.lab255;
 
+import java.util.Scanner;
+
 public class MyCalc {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        MyCalc pi = new MyCalc();
+        System.out.print("Input integer 'n' number: ");
+        pi.calcPi(scn.nextInt());
+    }
+
+    public static double calcPi(int n) {
+        if (n < 0) {
+            n = n * -1;
+        }
+        int constNum = 4;
+        int divider = 3;
+        double result = 4;
+        for (int i = 4; i <= n; i++) {
+            if (i % 2 == 0) {
+                result = result - ((double) constNum / (double) divider);
+            } else {
+                result = result + ((double) constNum / (double) divider);
+            }
+            divider += 2;
+        }
+        System.out.println("Value of computing pi approximately close to the real PI -> " + result);
+        return result;
+    }
+
 }
