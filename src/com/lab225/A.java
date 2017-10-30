f7bca993a4846507fbe851215a91fce99eed79a8
NPGroup <noodpgroup@gmail.com>
2017-10-30 04:11:14 +0200

    Lab 2.2.5

--- src/com/lab225/A.java
+++ src/com/lab225/A.java
@@ -1,4 +1,26 @@
 package com.lab225;
 
+import static java.lang.Math.PI;
+
 public class A {
+
+    private double square;
+
+    public void calcSquare(final int aRad,final int bSize) {
+        square = aRad * bSize;
+        System.out.println("Square of Rectangle: " + (int)square);
+    }
+
+
+    public void calcSquare(final int aRad) {
+        square = aRad * aRad;
+        System.out.println("Square of Foursquare: " + (int)square);
+
+    }
+
+
+    public void calcSquare(final double aRad) {
+        square = (aRad * aRad) * PI;
+        System.out.println("Square of Circle: " + square);
+    }
 }
---
+++ src/com/lab225/Main.java
@@ -0,0 +1,10 @@
+package com.lab225;
+
+public class Main {
+    public static void main(String[] args) {
+        A square = new A();
+        square.calcSquare(5);
+        square.calcSquare(2, 3);
+        square.calcSquare(2.0);
+    }
+}
